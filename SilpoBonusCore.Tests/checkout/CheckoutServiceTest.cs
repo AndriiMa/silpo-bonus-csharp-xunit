1078877439c73fc182fafddf12b1b0958e010f14
Andrew Maslyuk <andriima@tahometer.com>
2020-08-06 15:27:16 +0300

    Added test for CheckoutService.AddProduct()

--- SilpoBonusCore.Tests/checkout/CheckoutServiceTest.cs
+++ SilpoBonusCore.Tests/checkout/CheckoutServiceTest.cs
@@ -3,12 +3,15 @@ using Xunit;
 
 namespace SilpoBonusCore.Tests
 {
-    public class CheckoutServiceTest{
+    public class CheckoutServiceTest
+    {
 
         [Fact]
-        public void Close_Check_with_One_Product(){
+        public void Close_Check_with_One_Product()
+        {
             CheckoutService checkoutService = new CheckoutService();
             checkoutService.OpenCheck();
+
             checkoutService.AddProduct(new Product(7, "Milk", Category.MILK));
             Check check = checkoutService.CloseCheck();
 
@@ -16,14 +19,33 @@ namespace SilpoBonusCore.Tests
         }
 
         [Fact]
-        public void Close_Check_with_Two_Product(){
+        public void Close_Check_with_Two_Product()
+        {
             CheckoutService checkoutService = new CheckoutService();
             checkoutService.OpenCheck();
+
             checkoutService.AddProduct(new Product(7, "Milk", Category.MILK));
             checkoutService.AddProduct(new Product(3, "Bread"));
             Check check = checkoutService.CloseCheck();
 
             Assert.Equal(check.GetTotalCost(), 10);
+        }
+
+        [Fact]
+        public void Whem_AddProduct_Should_Add_Products_To_New_Check()
+        {
+            CheckoutService checkoutService = new CheckoutService();
+            checkoutService.OpenCheck();
+
+            checkoutService.AddProduct(new Product(7, "Milk", Category.MILK));
+            
+            Check milkCheck = checkoutService.CloseCheck();
+            Assert.Equal(milkCheck.GetTotalCost(), 7);
+
+            checkoutService.AddProduct(new Product(3, "Bread"));
+            Check breadCheck = checkoutService.CloseCheck();
+            Assert.Equal(breadCheck.GetTotalCost(), 3);
+
         }
 
 
